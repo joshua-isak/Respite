1e5e77122141fdc15dd8ac8ffe5ef02dfeb11e62
Joshua Isak <43322464+joshua-isak@users.noreply.github.com>
2021-01-04 20:21:47 -0500

    changed player mem allocation

--- respite/levels/level0/level0.cpp
+++ respite/levels/level0/level0.cpp
@@ -4,8 +4,16 @@
 
 
 void Level0::init() {
-    Player p;
-    addEntity(&p);
-    p.x = 7;
-    p.y = 8;
+    // Player p;
+    // addEntity(&p);
+    // p.x = 7;
+    // p.y = 8;
+
+    Player *p = new Player;
+    p->x = 7;
+    p->y = 9;
+
+    printf("x:%d y:%d\n", p->x, p->y);
+
+    addEntity(p);
 }
