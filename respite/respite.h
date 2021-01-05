eff4ed9657e1013ffb00f75fa0d5c2feaef31042
Joshua Isak <43322464+joshua-isak@users.noreply.github.com>
2021-01-04 19:19:08 -0500

    updated comment

--- respite/respite.h
+++ respite/respite.h
@@ -1,5 +1,5 @@
 // Header file containing all other header files main.cpp needs to include in order to link the code
-// in the respite folder. Basically all the code that is NOT the engine itself!
+// in the respite folder. Basically all the header code that is NOT part of the engine itself!
 
 #pragma once
 
