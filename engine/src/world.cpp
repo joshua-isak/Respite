9d596c0607844e64ba723fa4aeb71eaeae1480af
Joshua Isak <43322464+joshua-isak@users.noreply.github.com>
2021-01-09 00:09:49 -0500

    fixed collision code

--- engine/src/world.cpp
+++ engine/src/world.cpp
@@ -22,6 +22,7 @@ void World::addEntity (int id, Entity *e) {
 
 
 Entity* World::checkEntityCollision(Entity *self, float x_pos, float y_pos, float z_pos) {
+
     point a_max = { self->collisionBox.p_highest.x + x_pos,
                     self->collisionBox.p_highest.y + y_pos,
                     self->collisionBox.p_highest.z + z_pos  };
@@ -40,25 +41,25 @@ Entity* World::checkEntityCollision(Entity *self, float x_pos, float y_pos, floa
         // Don't check for a collision with ourselves
         if (other == self) { continue; }
 
-        point b_max = { other->collisionBox.p_highest.x + x_pos,
-                        other->collisionBox.p_highest.y + y_pos,
-                        other->collisionBox.p_highest.z + z_pos  };
+        point b_max = { other->collisionBox.p_highest.x + other->x,
+                        other->collisionBox.p_highest.y + other->y,
+                        other->collisionBox.p_highest.z + other->z  };
 
-        point b_min = { other->collisionBox.p_lowest.x + x_pos,
-                        other->collisionBox.p_lowest.y + y_pos,
-                        other->collisionBox.p_lowest.z + z_pos   };
+        point b_min = { other->collisionBox.p_lowest.x + other->x,
+                        other->collisionBox.p_lowest.y + other->y,
+                        other->collisionBox.p_lowest.z + other->z   };
 
         // Actually check if the collision boxes intersect anywhere
-        bool b1 = self->collisionBox.p_highest.x > other->collisionBox.p_lowest.x;
-        bool b2 = self->collisionBox.p_lowest.x  < other->collisionBox.p_highest.x;
+        bool b1 = a_max.x > b_min.x;
+        bool b2 = a_min.x < b_max.x;
 
-        bool b3 = self->collisionBox.p_highest.y > other->collisionBox.p_lowest.y;
-        bool b4 = self->collisionBox.p_lowest.y  < other->collisionBox.p_highest.y;
+        bool b3 = a_max.y > b_min.y;
+        bool b4 = a_min.y < b_max.y;
 
-        bool b5 = self->collisionBox.p_highest.z > other->collisionBox.p_lowest.z;
-        bool b6 = self->collisionBox.p_lowest.z  < other->collisionBox.p_highest.z;
+        bool b5 = a_max.z > b_min.z;
+        bool b6 = a_min.z < b_max.z;
 
-        if ( !(b1 && b2 && b3 && b4 && b5 && b6) ){
+        if (b1 && b2 && b3 && b4 && b5 && b6) {
             return other;
         }
     }
